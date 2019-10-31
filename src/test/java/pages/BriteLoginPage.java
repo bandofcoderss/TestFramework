7b82f52e97adce06c245edca737a0ef9fafb9a83
Ibraiym D <denizov@gmail.com>
2019-10-31 15:49:49 -0500

    Removed Manifold dependencies from POM.xml"

--- src/test/java/pages/BriteLoginPage.java
+++ src/test/java/pages/BriteLoginPage.java
@@ -2,7 +2,6 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import sun.plugin.services.BrowserService;
 import utils.BrowserUtils;
 import utils.Settings;
 
